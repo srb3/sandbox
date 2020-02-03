a7d6972407298477206cb35134050bab7bba5bb3
devoptimist <sbrown@chef.io>
2020-02-03 13:45:12 +0000

    update populate tf

--- populate/files/builder_populate/recipes/default.rb
+++ populate/files/builder_populate/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-directroy "#{node['builder_populate']['dir']}/on-prem-builder-master" do
+directory "#{node['builder_populate']['dir']}/on-prem-builder-master" do
   recursive true
 end
 
