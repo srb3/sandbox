0487e5a50e9312510997a7158f7798c3b2fb4e39
srb3 <sbrown@chef.io>
2020-02-27 23:32:53 +0000

    using version 0.0.14 of terraform chef habitat module

--- populate/main.tf
+++ populate/main.tf
@@ -67,7 +67,7 @@ locals {
 
 module "docker_host_prod" {
   source              = "srb3/habitat/chef"
-  version             = "0.0.13"
+  version             = "0.0.14"
   ips                 = [var.docker_host_prod_ip]
   instance_count      = 1
   user_name           = var.docker_host_user_name
@@ -82,7 +82,7 @@ module "docker_host_prod" {
 
 module "docker_host_dev" {
   source              = "srb3/habitat/chef"
-  version             = "0.0.13"
+  version             = "0.0.14"
   ips                 = [var.docker_host_dev_ip]
   instance_count      = 1
   user_name           = var.docker_host_user_name
